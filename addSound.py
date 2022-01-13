716c6a08d8aa76f43ec6da603e47be6653376c08
Lynda S Yearwood <lyearwood@lyearwood.local>
2022-01-13 11:05:54 -0500

    testing

---
+++ addSound.py
@@ -0,0 +1,24 @@
+import playsound
+from pygame import mixer
+import time
+
+mixer.init()
+sound1 = mixer.Sound("birds.mp3")
+sound2 = mixer.Sound("frogs.mp3")
+
+#mixer.music.load("frogs.mp3")
+
+mixer.Channel(0).play(sound2)
+time.sleep(3)
+mixer.Channel(1).play(sound2)
+answer = input("What is your name?")
+mixer.music.stop()
+print ("Hello, how are you?")
+
+time.sleep(5)
+mixer.music.play()
+input()
+
+# playsound.playsound("frogs.mp3", block=False)
+# print ("I love frogs")
+# answer = input("Type any key") 
